c7e6d0e6f16a9eb0132919b6805787c4349af2f8
Ronen Cohen <ronenc@gmail.com>
2015-09-05 22:21:41 +0300

    Add kibit.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,5 +3,6 @@
                   [lein-ancient "0.6.7"]
                   [lein-ring "0.9.6"]
                   [lein-environ "1.0.0"]
-                  [lein-cljsbuild "1.1.0"]]
+                  [lein-cljsbuild "1.1.0"]
+                  [lein-kibit "0.1.2"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}} 
