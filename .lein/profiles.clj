b5443e0cd99e7b7f1a5c33579d330ccc34c46990
Ronen Cohen <ronenc@gmail.com>
2015-09-05 19:36:11 +0300

    Add a few lein plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,7 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "1.1.0"]]
+                  [refactor-nrepl "1.1.0"]
+                  [lein-ancient "0.6.7"]
+                  [lein-ring "0.9.6"]
+                  [lein-environ "1.0.0"]
+                  [lein-cljsbuild "1.1.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}} 
