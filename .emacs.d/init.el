ddceac47241a08ae7cf37a2278b82a53f1d833f1
Ronen Cohen <ronenc@gmail.com>
2015-09-10 20:00:59 +0300

    Add ace-jump-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -50,6 +50,15 @@
 (add-my-package 'cljr-helm)
 (add-my-package 'magit)
 (add-my-package 'drag-stuff)
+(add-my-package 'ace-jump-mode)
+
+(require 'ace-jump-mode)
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
 
 (require 'drag-stuff)
 (drag-stuff-mode t)
