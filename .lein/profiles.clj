9a637bb59b26f9598e9052b0734270dac3e60828
Ronen Cohen <ronenc@gmail.com>
2016-02-04 22:55:48 +0200

    Update nrepl

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl "1.1.0"]
+{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]
                   [lein-ancient "0.6.7"]
                   [lein-ring "0.9.6"]
                   [lein-environ "1.0.0"]
                   [lein-cljsbuild "1.1.0"]
                   [lein-kibit "0.1.2"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}} 
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}} 
