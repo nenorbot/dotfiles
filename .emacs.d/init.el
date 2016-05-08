7e49d5f78954f2da3c5501ac91061f988677cf7b
Ronen Cohen <ronenc@gmail.com>
2016-05-08 11:22:26 +0300

    Show matching paren in minibuffer when it's offscreen

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -183,4 +183,14 @@
 
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 
-
+(defadvice show-paren-function
+  (after show-matching-paren-offscreen activate)
+   "If the matching paren is offscreen, show the matching line in the
+   echo area. Has no effect if the character before point is not of
+   the syntax class ')'."
+   (interactive)
+    (let* ((cb (char-before (point)))
+     (matching-text (and cb
+                     (char-equal (char-syntax cb) ?\) )
+                     (blink-matching-open))))
+     (when matching-text (message matching-text))))
