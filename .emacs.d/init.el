ff5738df9a5edfd1b88ca3880cda67bb77ddc6b0
Ronen Cohen <ronenc@gmail.com>
2016-05-10 17:41:20 +0300

    Fix clojure mode for better indentation inside Compojure routes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -143,6 +143,16 @@
   '(progn
      (define-key clojure-mode-map (kbd "C-c C-h") #'clojure-cheatsheet)))
 
+(define-clojure-indent
+  (defroutes 'defun)
+  (GET 2)
+  (POST 2)
+  (PUT 2)
+  (DELETE 2)
+  (HEAD 2)
+  (ANY 2)
+  (context 2))
+
 ;(defalias 'helm-buffer-match-major-mode 'helm-buffers-list--match-fn)
 
 (projectile-global-mode)
