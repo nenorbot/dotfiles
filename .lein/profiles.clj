a7d678800d6d5fa75780f25501c2070d07944486
Ronen Cohen <ronenc@gmail.com>
2016-07-25 16:40:01 +0300

    Upgrade cider-nrepl/refactor-nrepl/increase max perm size

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,12 @@
-{:user {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                  [refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [lein-ancient "0.6.7"]
-                  [lein-ring "0.9.6"]
-                  [lein-environ "1.0.0"]
-                  [lein-cljsbuild "1.1.0"]
-                  [lein-kibit "0.1.2"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}} 
+{:user {:plugins      [[cider/cider-nrepl "0.13.0"]
+                       [refactor-nrepl "2.3.0-SNAPSHOT"]
+                       [lein-ancient "0.6.7"]
+                       [lein-ring "0.9.6"]
+                       [lein-environ "1.0.3"]
+                       [lein-cljsbuild "1.1.0"]
+                       [lein-kibit "0.1.2"]
+                       [lein-repetition-hunter "0.1.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [repetition-hunter "1.0.0"]]
+	:jvm-opts     ["-XX:MaxPermSize=200M"]}
+ :env   {:squiggly {:checkers [:kibit]}}}
