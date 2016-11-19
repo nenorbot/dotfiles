30a5f6495e1f4cf55eb5698aaffbec7759222d71
Ronen Cohen <ronenc@gmail.com>
2016-11-19 11:12:26 +0200

    Upgrade cider-nrepl to 0.15.0-SNAPSHOT

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins      [[cider/cider-nrepl "0.13.0"]
+{:user {:plugins      [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
                        [refactor-nrepl "2.3.0-SNAPSHOT"]
                        [lein-ancient "0.6.7"]
                        [lein-ring "0.9.6"]
