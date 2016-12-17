fb94af73a2028ef31eddcbcce03a487bf8074574
Ronen Cohen <ronenc@gmail.com>
2016-12-17 21:06:12 +0200

    Rebind kill-buffer keybinding to kill-this-buffer

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -358,3 +358,5 @@
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 (setq emmet-move-cursor-between-quotes t)
+
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
