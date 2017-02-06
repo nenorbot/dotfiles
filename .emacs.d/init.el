e2a7306fad62b2ce3f9a7ab75a3c559fca136d86
Ronen Cohen <ronenc@gmail.com>
2017-02-06 12:27:33 +0200

    Add paredit mode to Scheme buffers

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -137,6 +137,7 @@
 (add-hook 'cider-repl-mode-hook 'enable-paredit-mode)
 (add-hook 'lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'scheme-mode-hook 'enable-paredit-mode)
 
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
