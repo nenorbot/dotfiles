34b1533959a832438b54d93c834a3376ded36a98
Ronen Cohen <ronen.cohen@coralogix.com>
2022-11-20 14:39:24 +0200

    wip

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -29,10 +29,12 @@
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
 
+(setq doom-font (font-spec :family "JetBrains Mono" :size 13))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-oceanic-next)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -74,6 +76,14 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(map! :leader
+      "v"
+      #'er/expand-region)
+
+(map! :leader
+      ";"
+      #'comment-line)
 
 (after! lsp-mode
   (setq lsp-rust-analyzer-server-display-inlay-hints t)
