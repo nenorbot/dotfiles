720e6194a1351c0cc9913c24798785bc8e29202f
Ronen Cohen <ronenc@gmail.com>
2023-05-19 18:59:27 +0300

    wip

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -153,7 +153,9 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pretty +journal +noter)      ; organize your plain life in plain text
+       (org +pretty +journal +noter
+            +dragndrop +gnuplot
+            +roam2 +present)      ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
