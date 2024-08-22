9dc70d465ebf05a1be867f82ba022c36c4ae5288
Ronen Cohen <ronen.cohen@coralogix.com>
2024-08-22 17:15:50 +0300

    wip

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -15,7 +15,7 @@
 ;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
 ;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-symbol-font' -- for symbols
 ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
@@ -29,16 +29,17 @@
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
 
-(setq doom-font (font-spec :family "JetBrains Mono" :size 15))
-
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-oceanic-next)
+(setq doom-font (font-spec :family "JetBrains Mono" :size 15))
+(modify-all-frames-parameters '((background-color . "#181818")))
+;; (add-to-list 'default-frame-alist '(background-color . "#181818"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type 'relative)
+(setq display-line-numbers-type t)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -77,6 +78,11 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+
 (map! :leader
       "v"
       #'er/expand-region)
@@ -85,10 +91,15 @@
 ;;       :desc "Comment/uncomment"
 ;;       ";"
 ;;       #'comment-line)
+;;
 
+;; unmap <SPC c x> and rebind it
 (map! :leader
-      "c ! e"
-      #'flycheck-explain-error-at-point)
+      :prefix ("c" . "code")
+      (:nv "x" nil
+       :prefix ("x". "Errors")
+        :desc "Explain" "e" #'flycheck-explain-error-at-point
+        :desc "List errors" "x" #'+default/diagnostics))
 
 (map! :leader
       :desc ";"
@@ -148,25 +159,26 @@
   (setq lsp-rust-analyzer-display-reborrow-hints "mutable")
   (setq lsp-rust-analyzer-display-chaining-hints t)
   (setq lsp-headerline-breadcrumb-enable '(project file symbols))
-  (require 'dap-cpptools)
-  (require 'dap-lldb)
-  (require 'dap-gdb-lldb)
+  ;; (require 'dap-cpptools)
+  ;; (require 'dap-lldb)
+  ;; (require 'dap-gdb-lldb)
 
-  (dap-register-debug-template
-   "Rust::LLDB Run Configuration"
-   (list :type "lldb-mi"
-         :request "launch"
-         :name "Rust::LLDB::Run"
-	 :gdbpath "rust-lldb"
-         :target nil
-         :cwd nil)))
+  ;; (dap-register-debug-template
+  ;;  "Rust::LLDB Run Configuration"
+  ;;  (list :type "lldb-mi"
+  ;;        :request "launch"
+  ;;        :name "Rust::LLDB::Run"
+  ;;        :gdbpath "rust-lldb"
+  ;;        :target nil
+  ;;        :cwd nil))
+  )
 
 
 ;; enable tree sitter parsing, text objects
-(add-hook 'rust-mode-hook #'tree-sitter-mode)
+;; (add-hook 'rust-mode-hook #'tree-sitter-mode)
 
 ;; enable tree sitter syntax highlighting
-(add-hook 'rust-mode-hook #'tree-sitter-hl-mode)
+;; (add-hook 'rust-mode-hook #'tree-sitter-hl-mode)
 
 (after! which-key
   (setq which-key-idle-delay 0.2)
@@ -176,23 +188,18 @@
 
 (add-hook 'window-setup-hook #'toggle-frame-fullscreen)
 
-(add-hook 'clojure-mode-hook #'evil-cleverparens-mode)
-(add-hook 'clojure-mode-hook #'highlight-indent-guides-mode)
-(after! clojure-mode
-  (require 'evil-cleverparens-text-objects))
+;; (add-hook! 'better-jumper-post-jump-hook #'recenter-top-bottom)
 
-(add-hook! 'better-jumper-post-jump-hook #'recenter-top-bottom)
-
-(add-hook 'lisp-mode-hook #'evil-cleverparens-mode)
-(after! lisp-mode
-  (require 'evil-cleverparens-text-objects))
+;; (add-hook 'lisp-mode-hook #'evil-cleverparens-mode)
+;; (after! lisp-mode
+;;   (require 'evil-cleverparens-text-objects))
 
 
 (display-time-mode)
 (setq avy-timeout-seconds 0.3)
 
-(global-undo-tree-mode)
-(add-hook 'evil-local-mode-hook 'turn-on-undo-tree-mode)
+;; (global-undo-tree-mode)
+;; (add-hook 'evil-local-mode-hook 'turn-on-undo-tree-mode)
 
 (setq evil-snipe-override-evil-repeat-keys nil)
 (setq doom-localleader-key ",")
@@ -204,66 +211,51 @@
     (insert ";")))
 
 
-(setq highlight-indent-guides-method 'bitmap)
-(setq highlight-indent-guides-responsive 'top)
-(setq highlight-indent-guides-delay 0)
-(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+;; (setq highlight-indent-guides-method 'bitmap)
+;; (setq highlight-indent-guides-responsive 'top)
+;; (setq highlight-indent-guides-delay 0)
+;; (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
 
 (setq math-additional-units '(
-  (GiB "1024 * MiB" "Giga Byte")
-  (MiB "1024 * KiB" "Mega Byte")
-  (KiB "1024 * B" "Kilo Byte")
-  (B nil "Byte")
-  (Gib "1024 * Mib" "Giga Bit")
-  (Mib "1024 * Kib" "Mega Bit")
-  (Kib "1024 * b" "Kilo Bit")
-  (b "B / 8" "Bit")))
-
-(add-hook
- 'treemacs-mode-hook
- (lambda ()
-   (setq-local hl-line-face 'custom-line-highlight)
-   (overlay-put hl-line-overlay 'face '((t (:background "gray40" :extend t))))
-   (treemacs--setup-icon-background-colors)))
+                              (GB "1024 * MiB" "Giga Byte")
+                              (MB "1024 * KiB" "Mega Byte")
+                              (KB "1024 * B" "Kilo Byte")
+                              (B nil "Byte")
+                              (Gib "1024 * Mib" "Giga Bit")
+                              (Mib "1024 * Kib" "Mega Bit")
+                              (Kib "1024 * b" "Kilo Bit")
+                              (b "B / 8" "Bit")))
 
 (setq doom-modeline-vcs-max-length 20)
 
 (setq-default right-margin-width 2)
 
-;; (map! :leader "j h" 'harpoon-quick-menu-hydra)
-;; (map! :leader "j a" 'harpoon-add-file)
-;; (map! :leader "j j" 'harpoon-toggle-quick-menu)
-;; (map! :leader "j c" 'harpoon-clear)
-;; (map! :leader "j f" 'harpoon-toggle-file)
-;; (map! :leader "1" 'harpoon-go-to-1)
-;; (map! :leader "2" 'harpoon-go-to-2)
-;; (map! :leader "3" 'harpoon-go-to-3)
-;; (map! :leader "4" 'harpoon-go-to-4)
-;; (map! :leader "5" 'harpoon-go-to-5)
-;; (map! :leader "6" 'harpoon-go-to-6)
-;; (map! :leader "7" 'harpoon-go-to-7)
-;; (map! :leader "8" 'harpoon-go-to-8)
-;; (map! :leader "9" 'harpoon-go-to-9)
-
-(after! undo-tree
-  (setq undo-tree-auto-save-history nil))
-
-;; (setq lsp-rust-analyzer-checkonsave-features '("all"))
+(add-hook 'dired-mode-hook 'dired-hide-details-mode)
 
 (setq evil-collection-setup-minibuffer t)
 
 (global-subword-mode)
 
-(setq org-noter-highlight-selected-text t)
+(setq magit-list-refs-sortby "-creatordate")
 
-(use-package! org-roam-ui
-    :after org-roam ;; or :after org
-;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-;;         a hookable mode anymore, you're advised to pick something yourself
-;;         if you don't care about startup time, use
-;;  :hook (after-init . org-roam-ui-mode)
-    :config
-    (setq org-roam-ui-sync-theme t
-          org-roam-ui-follow t
-          org-roam-ui-update-on-save t
-          org-roam-ui-open-on-start t))
+(setq better-jumper-add-jump-behavior 'replace)
+
+;; (after! corfu-mode
+;;   ;; TAB-and-Go customizations
+;;   (corfu-cycle t)           ;; Enable cycling for `corfu-next/previous'
+;;   (corfu-preselect 'prompt) ;; Always preselect the prompt
+;;   )
+
+;; ;; Use TAB for cycling, default is `corfu-complete'.
+;; (map! :after corfu-mode
+;;       :map corfu-map
+;;         "TAB" corfu-next
+;;         [tab] corfu-next
+;;         "S-TAB" corfu-previous
+;;         [backtab] corfu-previous
+;;         ;; ([tab] . corfu-next)
+;;         ;; ("S-TAB" . corfu-previous)
+;;         ;; ([backtab] . corfu-previous)
+;;         )
+
+;; ;;
